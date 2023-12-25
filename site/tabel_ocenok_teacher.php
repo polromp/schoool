735a60b043f0d53cbbccc40048b2e6fcbdacb2ab
Egor <egor297198@mail.ru>
2023-12-25 20:34:25 +0400

    Новые стили для табеля оценок учителя

--- site/tabel_ocenok_teacher.php
+++ site/tabel_ocenok_teacher.php
@@ -80,7 +80,7 @@ if ($mysqli->connect_error) {
     die('Ошибка подключения (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
-//$login = $_GET['var_name'] ?? '';
+//$login = $_GET['vaar_name'] ?? '';
 
 session_start();
 $login = $_SESSION['var_name'];
